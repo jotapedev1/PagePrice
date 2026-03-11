43f90547c258b7ab08522d536fb04ee919ad81d6
JoaoSantos5195 <jotape5195@gmail.com>
2026-03-10 22:37:20 -0300

    Starting Spring project with Java 21. Structuring foldes for MVC architecture, dividing front and backend in two differents directories. Setting properties for mongoDB

---
+++ Backend/src/main/java/com/pageprice/PagePriceApplication.java
@@ -0,0 +1,13 @@
+package com.pageprice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PagePriceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PagePriceApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/pageprice/PagePriceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pageprice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PagePriceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
