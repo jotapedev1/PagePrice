4f44a92c813fdbf9e0e2a64d8ed039bd765f9d30
JoaoSantos5195 <jotape5195@gmail.com>
2026-03-14 15:09:24 -0300

    updating pom.xml removing unused imports from model

--- Backend/src/main/java/com/pageprice/model/PriceModel.java
+++ Backend/src/main/java/com/pageprice/model/PriceModel.java
@@ -1,6 +1,5 @@
 package com.pageprice.model;
 
-import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
--- Backend/src/main/java/com/pageprice/model/ProductModel.java
+++ Backend/src/main/java/com/pageprice/model/ProductModel.java
@@ -1,6 +1,5 @@
 package com.pageprice.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
